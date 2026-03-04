65bc80d5073a625e842125bbb76e754b1fe500cc
kssrtej-eng <kssrtej@gmail.com>
2026-03-04 12:53:52 +0530

    Add files via upload
    # My Python Journey 🚀
    
    Hi, I am currently learning Python and building small projects to strengthen my programming skills.
    
    Projects so far:
    - CEO Age Calculator
    
    More coming soon.
    
    Started: 2026

---
+++ ceo_calculator.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+years_to_25 = 25 - age
+
+print("Hello world, this is", name)
+print(name, "has", years_to_25, "years to become a CEO.")
